9d9a5d966071028804b0805502ef07616a01f222
Dheeraj Nair <nair.dheeraj@yahoo.co.in>
2017-01-30 00:02:51 +0530

    Opens the browser to enter the login details.

--- main.py
+++ main.py
@@ -5,7 +5,18 @@ from bs4 import BeautifulSoup
 
 main_url = 'https://vtop.vit.ac.in/student/stud_login.asp'
 
-#web_data = str(ur.urlopen(main_url).read())
+web_data = str(ur.urlopen(main_url).read())
 #print(web_data)
 
 browser = webdriver.Firefox()
+browser.get(main_url)
+assert "FFCS" in browser.title
+
+reg_tb = browser.find_element_by_class_name("textbox2")
+pass_tb = browser.find_element_by_name("passwd")
+
+reg_no = input('Enter your registration number - ')
+password = input('Enter your password - ')
+
+reg_tb.send_keys(reg_no)
+pass_tb.send_keys(password)
