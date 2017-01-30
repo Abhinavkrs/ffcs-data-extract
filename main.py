ecfb2fe2ab73a9b18d213a49455b4f10aeb6cc7f
Dheeraj Nair <nair.dheeraj@yahoo.co.in>
2017-01-30 13:54:30 +0530

    Opening Captcha image.
    Added the code to open Captcha image and click the Login button on
    completion.

--- main.py
+++ main.py
@@ -2,10 +2,15 @@ from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 import urllib.request as ur
 from bs4 import BeautifulSoup
+from PIL import Image, ImageTk
+import urllib.parse as up
+import tkinter 
 
 main_url = 'https://vtop.vit.ac.in/student/stud_login.asp'
+reg_no = input('Enter your registration number - ')
+#password = input('Enter your password - ')
 
-web_data = str(ur.urlopen(main_url).read())
+#web_data = ur.urlopen(main_url).read()
 #print(web_data)
 
 browser = webdriver.Firefox()
@@ -15,8 +20,35 @@ assert "FFCS" in browser.title
 reg_tb = browser.find_element_by_class_name("textbox2")
 pass_tb = browser.find_element_by_name("passwd")
 
-reg_no = input('Enter your registration number - ')
-password = input('Enter your password - ')
-
 reg_tb.send_keys(reg_no)
 pass_tb.send_keys(password)
+
+#soup_data = BeautifulSoup(web_data,"html.parser")
+
+cap_img = browser.find_element_by_id("imgCaptcha")
+captcha_location = cap_img.location
+captcha_size = cap_img.size
+browser.save_screenshot('captcha.png')
+
+im = Image.open('captcha.png')
+left = captcha_location['x']
+top = captcha_location['y']
+right = captcha_location['x'] + captcha_size['width']
+bottom = captcha_location['y'] + captcha_size['height']
+
+im = im.crop((left, top, right, bottom))
+im.save('captcha.png')
+im.close()
+
+im = Image.open('captcha.png')
+root = tkinter.Tk()
+tkimage = ImageTk.PhotoImage(im)
+tkinter.Label(root, image=tkimage).pack()
+root.mainloop()
+
+captcha_data = input('Enter the Captcha - ')
+captcha_tb = browser.find_element_by_name("vrfcd")
+captcha_tb.send_keys(captcha_data)
+
+submit_btn = browser.find_element_by_class_name("submit3")
+submit_btn.click()
